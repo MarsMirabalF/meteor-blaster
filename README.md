66df7c7830d8d910d2313e8822ff884fba829808
MarsMirabalF <marcelomirabalferrel@gmail.com>
2026-04-07 21:11:09 -0400

    Feat: Se realizo la implementacion de movimiento para la nave en el canvas con las teclas W,A,S,D. Fix: Se cambiaron los colores de la nave y el canvas. Update: Se actualizo la informacion de todo lo utilizado en el markdown.

--- README.md
+++ README.md
@@ -5,8 +5,20 @@ A continuacion se listaran sitios web de referencia:
 
 Siyio web parecido a W3 schools pero para la API de canvas: 
 https://developer.mozilla.org/es/docs/Web/API/Canvas_API/Tutorial
-- Se uso para averiguar funciones especificas como : canvas.getContext("2d");
+    -Se uso para averiguar funciones especificas como : canvas.getContext("2d");
+
+Sitio web de donde se utilizo para aprender a manejar que valores tienen las teclas:
+https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
+    -Se uso para: if (e.key in teclas) teclas[e.key] = true;
+
+Sitio web para saber como escuchar eventos: 
+https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+    -Se uso para : document.addEventListener("keydown", (e) =>
+
+Sitio web para aprender de animacion y el bucle del juego: 
+https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+    -Se uso para requestAnimationFrame(juegoEnCurso);
 
 Tutorial de como hacer el juego Space Invaders:
 https://youtu.be/v74X5dtDLvg?si=ofvZAOHHB9Zcrko2
-    -De aqui por ahora se uso para dibujar la nave.
+    -De aqui por ahora se uso para dibujar la nave y ahora el movimiento con las teclas.
--- juego.js
+++ juego.js
@@ -8,28 +8,86 @@ const nave = {
     x: canvas.width / 2,
     y: canvas.height / 2,
     tamanio: 20,
-    color: "#00ff99"
+    color: "#befffb",
+    angulo: 0,
+    velocidadX: 0,
+    velocidadY: 0
 };
 
-function dibujarNave(x, y, tamanio) {
+const teclas = {
+    a: false,
+    d: false,
+    w: false,
+    s: false
+};
+
+document.addEventListener("keydown", (e) => {
+    if (e.key in teclas) teclas[e.key] = true;
+});
+document.addEventListener("keyup", (e) => {
+    if (e.key in teclas) teclas[e.key] = false;
+});
+
+function actualizar() {
+    if (teclas.a) {
+        nave.angulo -= 0.05;
+    }
+    if (teclas.d) {
+        nave.angulo += 0.05;
+    }
+    if (teclas.w) {
+        nave.velocidadX += Math.sin(nave.angulo) * 0.2;
+        nave.velocidadY -= Math.cos(nave.angulo) * 0.2;
+    }
+    if (teclas.s) {
+        nave.velocidadX -= Math.sin(nave.angulo) * 0.2;
+        nave.velocidadY += Math.cos(nave.angulo) * 0.2;
+    }
+
+    nave.x += nave.velocidadX;
+    nave.y += nave.velocidadY;
+
+    nave.velocidadX *= 0.98;
+    nave.velocidadY *= 0.98;
+
+    if (nave.x > canvas.width){ 
+        nave.x = 0; 
+    }
+    if (nave.x < 0){ 
+        nave.x = canvas.width; 
+    }
+    if (nave.y > canvas.height){ 
+        nave.y = 0; 
+    }
+    if (nave.y < 0){ 
+        nave.y = canvas.height; 
+    }
+}
+
+function dibujarNave(x, y, tamanio, angulo) {
     ctx.save();
     ctx.translate(x, y);
-
+    ctx.rotate(angulo);
     ctx.beginPath();
     ctx.moveTo(0, -tamanio);
     ctx.lineTo(tamanio * 0.8, tamanio);
     ctx.lineTo(0, tamanio * 0.5);
     ctx.lineTo(-tamanio * 0.8, tamanio);
     ctx.closePath();
-
     ctx.strokeStyle = nave.color;
     ctx.lineWidth = 2;
     ctx.stroke();
-
     ctx.restore();
 }
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+function juegoEnCurso() {
+    ctx.fillStyle = "#000a27";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-dibujarNave(nave.x, nave.y, nave.tamanio);
+    actualizar();
+    dibujarNave(nave.x, nave.y, nave.tamanio, nave.angulo);
+
+    requestAnimationFrame(juegoEnCurso);
+}
+
+juegoEnCurso();
